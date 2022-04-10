a4273d22f9e358ab2531c5d7eafae8d636349e65
Andreas Wölbl <andwoelbl@edu.aau.at>
2022-04-10 20:24:08 +0200

    Initial commit for the server of the project Chessify

---
+++ src/test/java/at/aau/se2/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package at.aau.se2.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
