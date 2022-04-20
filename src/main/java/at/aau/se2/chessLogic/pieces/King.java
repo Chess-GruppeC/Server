baaabb04b40fbd10d11f067b3bb2bed27b7dd5aa
Phalangs <p1miklautz@edu.aau.at>
2022-04-20 20:58:06 +0200

    getting rid of empty if condition in King

--- src/main/java/at/aau/se2/chessLogic/pieces/King.java
+++ src/main/java/at/aau/se2/chessLogic/pieces/King.java
@@ -26,9 +26,10 @@ public class King extends ChessPiece{
                     if(board.getPieceAtLocation(new Location(i, j))==null) {    //if empty space, add move option to legal moves
                         legalMoveTargetList.add(new Location(i, j));
                     }else
-                    if(board.getPieceAtLocation(new Location(i, j)).getColour()==this.getColour()){
+                        //don't need that middle part because nothing happens, can just leave it out cause else-if
+                    /*if(board.getPieceAtLocation(new Location(i, j)).getColour()==this.getColour()){
                                                                          //if space is occupied from friendly piece, don't add
-                    }else
+                    }else*/
                     if(board.getPieceAtLocation(new Location(i, j)).getColour()!=this.getColour()){
                         legalMoveTargetList.add(new Location(i,j));
                                                                          //if space is occupied from enemy piece, give option to take
