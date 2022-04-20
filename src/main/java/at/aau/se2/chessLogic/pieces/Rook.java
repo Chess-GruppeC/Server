92dfeb8f1997f97db6a424ff7f271f38c4809537
Phalangs <p1miklautz@edu.aau.at>
2022-04-20 21:26:02 +0200

    implemented getLegalMoves for Rooks

--- src/main/java/at/aau/se2/chessLogic/pieces/Rook.java
+++ src/main/java/at/aau/se2/chessLogic/pieces/Rook.java
@@ -14,6 +14,75 @@ public class Rook extends ChessPiece{
 
     @Override
     public ArrayList<Location> getLegalMoves(ChessBoard board) {
-        return null;
+        ArrayList<Location> legalMoveTargetList = new ArrayList<>();
+
+        Location pieceLocation=board.getLocationOf(this);
+
+        //check up
+        for(int i=pieceLocation.getRow(), j=pieceLocation.getColumn()-1;j>=0;j--){
+
+            if(board.getPieceAtLocation(new Location(i, j))==null) {    //if empty space, add move option to legal moves
+                legalMoveTargetList.add(new Location(i, j));
+            }else
+            if(board.getPieceAtLocation(new Location(i, j)).getColour()==this.getColour()){
+                break;                                                 //if space is occupied from friendly piece, break
+            }else
+            if(board.getPieceAtLocation(new Location(i, j)).getColour()!=this.getColour()){
+                legalMoveTargetList.add(new Location(i,j));
+                break;                                                 //if space is occupied from enemy piece, give option to take and break
+            }
+
+        }
+
+        //check down
+        for(int i=pieceLocation.getRow(), j=pieceLocation.getColumn()+1;j<8;j++){
+
+            if(board.getPieceAtLocation(new Location(i, j))==null) {    //if empty space, add move option to legal moves
+                legalMoveTargetList.add(new Location(i, j));
+            }else
+            if(board.getPieceAtLocation(new Location(i, j)).getColour()==this.getColour()){
+                break;                                                 //if space is occupied from friendly piece, break
+            }else
+            if(board.getPieceAtLocation(new Location(i, j)).getColour()!=this.getColour()){
+                legalMoveTargetList.add(new Location(i,j));
+                break;                                                 //if space is occupied from enemy piece, give option to take and break
+            }
+
+        }
+
+        //check left
+        for(int i=pieceLocation.getRow()-1, j=pieceLocation.getColumn();i>=0;i--){
+
+            if(board.getPieceAtLocation(new Location(i, j))==null) {    //if empty space, add move option to legal moves
+                legalMoveTargetList.add(new Location(i, j));
+            }else
+            if(board.getPieceAtLocation(new Location(i, j)).getColour()==this.getColour()){
+                break;                                                 //if space is occupied from friendly piece, break
+            }else
+            if(board.getPieceAtLocation(new Location(i, j)).getColour()!=this.getColour()){
+                legalMoveTargetList.add(new Location(i,j));
+                break;                                                 //if space is occupied from enemy piece, give option to take and break
+            }
+
+        }
+
+        //check right
+        for(int i=pieceLocation.getRow()+1, j=pieceLocation.getColumn();i<8;i++){
+
+            if(board.getPieceAtLocation(new Location(i, j))==null) {    //if empty space, add move option to legal moves
+                legalMoveTargetList.add(new Location(i, j));
+            }else
+            if(board.getPieceAtLocation(new Location(i, j)).getColour()==this.getColour()){
+                break;                                                 //if space is occupied from friendly piece, break
+            }else
+            if(board.getPieceAtLocation(new Location(i, j)).getColour()!=this.getColour()){
+                legalMoveTargetList.add(new Location(i,j));
+                break;                                                 //if space is occupied from enemy piece, give option to take and break
+            }
+
+        }
+
+
+        return legalMoveTargetList;
     }
 }
