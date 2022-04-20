f338b272828953c2fb62e5a8920b26375a4f414b
Phalangs <p1miklautz@edu.aau.at>
2022-04-20 21:25:33 +0200

    added second option for both Pawn versions

--- src/main/java/at/aau/se2/chessLogic/pieces/Pawn.java
+++ src/main/java/at/aau/se2/chessLogic/pieces/Pawn.java
@@ -31,6 +31,41 @@ public class Pawn extends ChessPiece {
         int j;
 
         //move forward
+        i=row;
+        j=column+1;
+        if(board.isWithinBounds(row,j)){
+            if(board.getPieceAtLocation(new Location(i, j))==null) {    //if empty space, add move option to legal moves
+                legalMoveTargetList.add(new Location(i, j));
+
+                if(this.moved==false){                                  //if empty space and not moved
+                    j++;                                                //check also if next tile is free
+                    if(board.isWithinBounds(row,j)) {
+                        if (board.getPieceAtLocation(new Location(i, j)) == null) {    //if empty space, add move option to legal moves
+                            legalMoveTargetList.add(new Location(i, j));
+                        }
+                    }
+                }
+
+            }
+        }
+
+        //check for taking left and right
+        i=row-1;
+        j=column+1;
+        if(board.isWithinBounds(row,j)){
+            if(board.getPieceAtLocation(new Location(i, j)).getColour()!=this.getColour()){
+                legalMoveTargetList.add(new Location(i,j));
+                //if space is occupied from enemy piece, give option to take
+            }
+        }
+        i=row+1;
+        j=column+1;
+        if(board.isWithinBounds(row,j)){
+            if(board.getPieceAtLocation(new Location(i, j)).getColour()!=this.getColour()){
+                legalMoveTargetList.add(new Location(i,j));
+                //if space is occupied from enemy piece, give option to take
+            }
+        }
 
         return legalMoveTargetList;
     }
